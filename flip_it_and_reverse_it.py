b1d78d8ccdb74558d11a123f513bb23565888e03
Kathleen Tang <kathleen@Kathleens-MacBook-Pro.local>
2017-05-04 18:39:38 -0700

    Here we go again

--- flip_it_and_reverse_it.py
+++ flip_it_and_reverse_it.py
@@ -1,3 +1,7 @@
+#Write a function that will take a given string124 and reverse the order of the words. 
+#"Hello world" becomes "world Hello" and "May the Fourth be with you" becomes "you with be Fourth the May"
+
+
 def reverse_it(string):
     rv = []
     for word in string.split():
